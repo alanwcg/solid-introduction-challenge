ccc1d00f74cec82d8eb1e3715e45988716a6c25f
alanwcg <alancintra7@gmail.com>
2021-03-20 14:59:00 -0300

    Challenge Completed

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,55 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    const user = this.users.find((user) => user.email === email);
+
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    let user: User;
+
+    const updatedUsers = this.users.map((item) => {
+      if (item.id === receivedUser.id) {
+        user = item;
+
+        user.admin = true;
+        user.updated_at = new Date();
+
+        return user;
+      }
+
+      return item;
+    });
+
+    this.users = updatedUsers;
+
+    return user;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
